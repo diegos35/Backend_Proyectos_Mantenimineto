f3d897d0d45cfa06534162708b0f0671694e25b6
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2020-11-18 13:23:30 -0500

    Feat: Creando migracion para terceros

---
+++ database/migrations/2020_11_18_165039_create_terceros_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTercerosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terceros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('tipo_documento_id')->comment('llave foranea de elementos de lista');
+            $table->string('numero_documento', 20);
+            $table->string('nombre1', 40);
+            $table->string('nombre2', 40);
+            $table->string('apellido1', 40);
+            $table->string('apellido2', 40);
+            $table->unsignedInteger('genero_id')->comment('llave foranea de elemento de la lista');
+            $table->tinyInteger('activo')->default('1');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terceros');
+    }
+}
